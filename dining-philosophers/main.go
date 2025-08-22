aa25697ff61e9a3975326c513117b0b5729206c0
Annis Souames <Mohamed-Annis.Souames@telekom.de>
2025-08-22 21:33:59 +0200

    Simplified chopsticks to mutexes

--- dining-philosophers/main.go
+++ dining-philosophers/main.go
@@ -6,61 +6,36 @@ import (
 	"time"
 )
 
-
-type Chopstick struct {
-	id     int
-	state bool
-	mux    sync.Mutex
-}
-
-// Hold will set the chopstick state to true and 
-func (cp *Chopstick) Hold(philId int) {
-	cp.mux.Lock()
-	cp.state = true
-	// Logging to make sure that a chopstick is not being used twice by different philosophers
-	fmt.Printf("Philo of ID %d is holding chopstick of ID %d", philId, cp.id)
-}
-
-// PutDown will set the state to false and unlock the chopstick
-func (cp *Chopstick) PutDown(philId int) {
-	cp.state = false
-	// Logging to make sure that a chopstick is not put down twice by different philosophers
-	fmt.Printf("Philo of ID %d is putting down chopstick of ID %d", philId, cp.id)
-	// Unlock the mutex to allow other philosophers to use the chopstick
-	cp.mux.Unlock()
-}
 type Philosopher struct {
 	id             int
-	leftChopstick  *Chopstick
-	rightChopstick *Chopstick
+	leftChopstick  *sync.Mutex
+	rightChopstick *sync.Mutex
 }
 
 func (p *Philosopher) Think() {
-	p.leftChopstick.PutDown(p.id)
-	p.rightChopstick.PutDown(p.id)
-	fmt.Printf("Philosopher %d is thinking and has put down his chopsticks", p.id)
+	fmt.Printf("Philosopher %d is thinking for 200ms", p.id)
 	time.Sleep(200 * time.Millisecond)
 }
 
 func (p *Philosopher) Eat() {
-	p.leftChopstick.Hold(p.id)
-	p.rightChopstick.Hold(p.id)
-	fmt.Printf("Philosopher %d is eating", p.id)
+	p.leftChopstick.Lock()
+	defer p.leftChopstick.Unlock()
+	p.rightChopstick.Lock()
+	defer p.rightChopstick.Unlock()
+	fmt.Printf("Philosopher %d is eating for 200ms", p.id)
 	time.Sleep(200 * time.Millisecond)
 }
 
 // Live is just a wrapper method for the eat and think activities.
-// It is important to start with eat activity to lock the mutexes, starting with think activity will cause a runtime error because we are trying to unlock a non-locked mutex.
 func (p *Philosopher) Live(){
-	p.Eat()
-	p.Think()
+	for{ //  A philosopher life is an endless loop of eating and thinking
+		p.Eat()
+		p.Think()
+	}
 }
-
-
 
 func main() {
 	// Create 5 philosophers
-	// Create 5 chopsticks
-	// Loop around the 5 infinitely in a circular way and run `go Live()`
+	
 
 }
